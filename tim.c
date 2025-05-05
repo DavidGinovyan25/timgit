efdfda4544f127ebf43466d88e653f2209f4f340
DavidGinovyan25 <david48952@gmail.com>
2025-05-05 15:03:32 +0200

    feat: add summatot function

--- tim.c
+++ tim.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+__int32_t summator(int x, int y) {
+    return x + y;
+}
+
+
 int main() {
     int x = 2;
-    int y  = 3;
+    int y = 5;
+    int z = summator(x, y);
     printf("%d", x + y);
     printf("%s", "timgay\n");
     return 0;
